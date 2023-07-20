2dbfe45427dcf40cff9140ac960f1f8fbe1ace06
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-07-20 15:42:22 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 import ContactList from './ContactList';
+import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter';
-import ContactForm from './ContactForm';
 
 import css from './App.module.css';
 import Message from './Message';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
+  const addContact = ({ name, number }) => {
     const newContact = { id: nanoid(), name, number };
-
-    contacts.some(contact => contact.name === name)
-      ? Report.warning(
-          `${name}`,
-          'This user is already in the contact list.',
-          'OK'
-        )
-      : this.setState(({ contacts }) => ({
-          contacts: [newContact, ...contacts],
-        }));
+    if (contacts.some(contact => contact.name === name)) {
+      Report.warning(
+        `${name}`,
+        'This user is already in the contact list.',
+        'OK'
+      );
+    } else {
+      setContacts(prevContacts => [newContact, ...prevContacts]);
+    }
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  filtredContacts = () => {
-    const { filter, contacts } = this.state;
+  const filteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  componentDidMount() {
-    const stringifiedContacts = localStorage.getItem('contacts');
-    const contacts = JSON.parse(stringifiedContacts) ?? [];
-    this.setState({ contacts });
-  }
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
+    localStorage.setItem('contacts', stringifiedContacts);
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      const stringifiedContacts = JSON.stringify(this.state.contacts);
-      localStorage.setItem('contacts', stringifiedContacts);
-    }
-  }
-
-  render() {
-    const { filter } = this.state;
-    const addContact = this.addContact;
-    const deleteContact = this.deleteContact;
-    const changeFilter = this.changeFilter;
-    const filtredContacts = this.filtredContacts();
-
-    return (
-      <div className={css.container}>
-        <h1 className={css.title}>
-          Phone<span className={css.title__color}>book</span>
-        </h1>
-        <ContactForm onSubmit={addContact} />
-        <h2 className={css.subtitle}>Contacts</h2>
-        <Filter filter={filter} changeFilter={changeFilter} />
-        {this.state.contacts.length > 0 ? (
-          <ContactList
-            contacts={filtredContacts}
-            onDeleteContact={deleteContact}
-          />
-        ) : (
-          <Message text="Contact list is empty." />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>
+        Phone<span className={css.title__color}>book</span>
+      </h1>
+      <ContactForm onSubmit={addContact} />
+      <h2 className={css.subtitle}>Contacts</h2>
+      <Filter filter={filter} changeFilter={changeFilter} />
+      {contacts.length > 0 ? (
+        <ContactList
+          contacts={filteredContacts()}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <Message text="Contact list is empty." />
+      )}
+    </div>
+  );
+};
